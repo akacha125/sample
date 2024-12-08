6239fb4c59d180b6a219849212cc3a42dd6cfd09
MadhanBlast <mk3696594@gmail.com>
2024-12-08 23:16:02 +0530

    up

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import WelcomeAnimation from "@/components/WelcomeAnimation";
 import useFectchData from "@/hooks/useFetchData";
 import Head from "next/head";
